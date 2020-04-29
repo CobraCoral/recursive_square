0a7c72654d8234f1590240e4050e9dd97a0b1b46
Fernando C. Jeronymo <fernando.fcavalcanti@gmail.com>
2020-04-29 15:47:10 -0400

    Added unittests Fixed a bug found while running unittests (tail function was not working properly for negative numbers due to a suboptimization we added) Recursive Square was also returning backward/forward progression numbers inverted for negative number

--- recursive_square.py
+++ recursive_square.py
@@ -5,9 +5,7 @@ from functools import wraps
 
 def static_vars(**kwargs):
     """Add static variables to functions."""
-    # print("static_vars", kwargs)
     def wrap(func):
-        # print("inside static_vars.wrap")
         for k in kwargs:
             setattr(func, k, kwargs[k])
         return func
@@ -16,13 +14,11 @@ def static_vars(**kwargs):
 
 def memoization_decorator(func):
     """Add simple memoization to a function."""
-    # print("memoization_decorator")
     @wraps(func)
     def wrapper_decorator(*args, **kwargs):
         number = args[0]
         if number in func.square_cache.keys():
             return func.square_cache[number]
-        # print("%s: memoized %s"%(__name__, number))
         square = func(*args, **kwargs)
         func.square_cache[number] = square
         return square
@@ -32,19 +28,17 @@ def memoization_decorator(func):
 @memoization_decorator
 @static_vars(square_cache={0: (-1, 1, 0)})
 def recursive_square(number):
-    # print("recursive_square")
     """Compute the square of N as described by the recursive algorithm."""
     if number < 0:
         bwd_prog, fwd_prog, square = recursive_square(-number)
-        return (-fwd_prog, -bwd_prog, square)
+        return (-bwd_prog, -fwd_prog, square)
     if number == 0:
         return (-1, 1, 0)
     n_minus1_bwd_prog, n_minus1_fwd_prog, n_minus1_square = recursive_square(number - 1)
     n_bwd_prog = n_minus1_bwd_prog + 2
     n_fwd_prog = n_minus1_fwd_prog + 2
-    n_square = n_minus1_fwd_prog + n_minus1_square
+    n_square = n_minus1_square + n_minus1_fwd_prog
     result = (n_bwd_prog, n_fwd_prog, n_square)
-    # print("recursive", number, result)
     return result
 
 
@@ -52,35 +46,30 @@ def recursive_square(number):
 @static_vars(square_cache={0: (-1, 1, 0)})
 def recursive_square_tail(number):
     """Compute tail recursive square of N."""
-    # print("---> recursive square tail", number)
-
-    # Another small optimization. If we already have the previous number,
-    # just use it using the fwd progression available
-    if number - 1 in recursive_square_tail.square_cache.keys():
-        bwd_prog, fwd_prog, square = recursive_square_tail.square_cache[number - 1]
-        return (bwd_prog + 2, fwd_prog + 2, square + fwd_prog)
-
     def recursive_square_tail_full(number, idx, bprog, fprog, square):
-        # print('rsqt: ', number, idx, bprog, fprog, square)
         if number < 0:
             bwd_prog, fwd_prog, square = recursive_square_tail(-number)
             return (-bwd_prog, -fwd_prog, square)
 
-        # small optimization
-        if idx not in recursive_square_tail.square_cache.keys():
-            recursive_square_tail.square_cache[idx] = (bprog, fprog, square)
-        #     print("    memoized", number, idx, bprog, fprog, square)
-        # else:
-        #     print("    memoiz", number, idx, bprog, fprog, square)
-
-        # print("    full", number, idx, bprog, fprog, square)
+        ## small optimization
+        #if idx not in recursive_square_tail.square_cache.keys():
+        #    recursive_square_tail.square_cache[idx] = (bprog, fprog, square)
         if number == 0:
             return (bprog, fprog, square)
         return recursive_square_tail_full(number - 1,
                                           idx + 1,
                                           bprog + 2,
                                           fprog + 2,
-                                          bprog + square + 2)
+                                          square + bprog + 2)
+
+    ## Another small optimization. If we already have the previous number,
+    ## just use it using the fwd progression available
+    if number - 1 in recursive_square_tail.square_cache.keys():
+        bwd_prog, fwd_prog, square = recursive_square_tail.square_cache[number - 1]
+        if number < 0:
+            return (bwd_prog + 2, fwd_prog + 2, square + bwd_prog)
+        return (bwd_prog + 2, fwd_prog + 2, square + fwd_prog)
+
     return recursive_square_tail_full(number, 0, -1, 1, 0)
 
 
@@ -104,47 +93,3 @@ def iterative_square(number):
     if negative:
         return (-bwd_prog, -fwd_prog, square)
     return (bwd_prog, fwd_prog, square)
-
-
-if __name__ == "__main__":
-    # print("about to call recursive_square")
-    print(3, iterative_square(-3))
-    print(3, recursive_square(3))
-    print(3, recursive_square_tail(3))
-    print(10, recursive_square_tail(10))
-    print(9, recursive_square_tail(9))
-    print(4, recursive_square_tail(4))
-    print(-3, recursive_square_tail(-3))
-    print(5, recursive_square_tail(5))
-    for n in range(-10, 11, 1):
-        print(n, recursive_square_tail(n))
-
-    def doit(forward=True):
-        """Compute recursion and tail recursion, and prints results."""
-        def compare(label, left, right, num):
-            fmt_short = 'n^2({:^5}) = {:>5}  ... (bwd:{:^5}, fwd:{:^5})'
-            fmt_long = '%s       ....     %s' % (fmt_short, fmt_short)
-            if left != right:
-                print("NOK",
-                      label,
-                      fmt_long.format(num, recur[2], recur[0], recur[1],
-                                      num, tail[2], tail[0], tail[1]))
-            else:
-                print(" OK",
-                      label,
-                      fmt_short.format(num, recur[2], recur[0], recur[1]))
-        if forward:
-            range_is = range(1, 51)
-        else:
-            range_is = range(50, 0, -1)
-        for num in range_is:
-            recur = recursive_square(num)
-            tail = recursive_square_tail(num)
-            iterative = iterative_square(num)
-
-            compare("recur x      tail", recur, tail, num)
-            compare("recur x iterative", recur, iterative, num)
-    doit()
-    doit(False)
-    doit()
-    doit(False)
---
+++ recursive_square_unittest.py
@@ -0,0 +1,41 @@
+#!/bin/env python3
+"""Test knapsack algorithm implementation."""
+import unittest
+from recursive_square import iterative_square, recursive_square, recursive_square_tail
+
+class SquareTestCommon():
+    def setUp(self):
+        self.range_start = -2
+        self.range_end = 2 + 1
+        self.function = None
+
+    def test(self):
+        self.assertIsNotNone(self.function)
+        #print('{:>4} --> {:>4} {:>4} {:>4}'.format('N', 'BP', 'FP', 'Square'))
+        for N in range(self.range_start, self.range_end):
+            with self.subTest(N=N):
+                bwd_prog, fwd_prog, computed_N = self.function(N)
+                #print('{:>4} --> {:>4} {:>4} {:>4}'.format(N, bwd_prog, fwd_prog, computed_N))
+                self.assertEqual(computed_N, N**2)
+
+
+class RecursiveSquareTest(unittest.TestCase, SquareTestCommon):
+    def setUp(self):
+        SquareTestCommon.setUp(self)
+        self.function = recursive_square
+
+
+class RecursiveSquareTailTest(unittest.TestCase, SquareTestCommon):
+    def setUp(self):
+        SquareTestCommon.setUp(self)
+        self.function = recursive_square_tail
+
+
+class IterativeSquareTest(unittest.TestCase, SquareTestCommon):
+    def setUp(self):
+        SquareTestCommon.setUp(self)
+        self.function = iterative_square
+
+
+if __name__ == '__main__':
+    unittest.main()
